ecd8854a61ecacf8f42b1f175edf8fae12b0661e
ishitabee <ishitabiswal333@gmail.com>
2026-06-12 17:11:51 +0530

    Add files via upload

---
+++ SumRecursion.java
@@ -0,0 +1,21 @@
+//Calculation sum of n numbers using recursion
+
+import java.util.Scanner;
+public class SumRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = RecursionSum(num);
+        System.out.println("The sum of the first " + num + " numbers is: " + sum);
+        sc.close();
+    }
+
+    public static int RecursionSum(int num) {
+        if (num <= 0) {
+            return 0;
+        }
+        return num + RecursionSum(num - 1);
+    }
+}
+
