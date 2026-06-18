d33e624e7a94ac92bdc12b6f3e87e51cfa464150
ishitabee <ishitabiswal333@gmail.com>
2026-06-18 18:13:13 +0530

    Refactor SelectionSort for variable naming consistency
    Updated variable name for consistency and corrected output.

--- SelectionSort.java
+++ SelectionSort.java
@@ -7,28 +7,28 @@ public class SelectionSort{
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
 
-        System.out.println("Enter the number of elements in the array:");     //user input for size of array
+        System.out.println("Enter the number of elements in the array:");       //user input for size of array
         int sizeOfArray= sc.nextInt();
 
-        int[] myarray= new int[sizeOfArray];                                   //creating array with user given size
+        int[] myArray= new int[sizeOfArray];                                   //creating array with user given size
 
 
-        System.out.println("Enter the elements of the array:");              //user input for elements of array
+        System.out.println("Enter the elements of the array:");                //user input for elements of array
         for(int element=0; element<sizeOfArray; element++){
-            myarray[element]= sc.nextInt();
+            myArray[element]= sc.nextInt();
         }
 
 
         for(int i=0; i<sizeOfArray-1; i++){                                       
             int SmallestIndex= i;
             for(int j=i+1; j<sizeOfArray; j++){
-                if(myarray[j]<myarray[SmallestIndex]){
+                if(myArray[j]<myArray[SmallestIndex]){
                     SmallestIndex= j;
                 }
             }
-            int temp= myarray[SmallestIndex];
-            myarray[SmallestIndex]= myarray[i];
-            myarray[i]= temp;
+            int temp= myArray[SmallestIndex];
+            myArray[SmallestIndex]= myArray[i];
+            myArray[i]= temp;
         }
 
         System.out.println("Sorted array:");                                              
